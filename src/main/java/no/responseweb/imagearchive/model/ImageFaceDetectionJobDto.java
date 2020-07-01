1ebbcddc8957a9f06fd6bb122111d2d56227643e
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-07-01 21:39:50 +0200

    Cleaned store-service Moved Metadata Extraction to own microservice

---
+++ src/main/java/no/responseweb/imagearchive/model/ImageFaceDetectionJobDto.java
@@ -0,0 +1,16 @@
+package no.responseweb.imagearchive.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ImageFaceDetectionJobDto {
+    private UUID fileStoreId;
+}
